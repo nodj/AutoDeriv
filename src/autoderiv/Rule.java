c4628b65a744be42d37acdc59e2774df038b2742
johan duparc <johan.duparc@gmail.com>
2014-12-01 11:37:50 +0100

    added virtual method in rule, to handle a single resource

--- src/autoderiv/Rule.java
+++ src/autoderiv/Rule.java
@@ -1,7 +1,9 @@
 package autoderiv;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public interface Rule {
 	public void applyOnProject(IProgressMonitor progress);
+	public void applyOnResource(IResource res, IProgressMonitor progress);
 }
