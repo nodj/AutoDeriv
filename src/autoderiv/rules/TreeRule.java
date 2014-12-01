9927227b6cb546a27c6d9f1923a3c2b0c8bf2124
johan duparc <johan.duparc@gmail.com>
2014-12-01 11:36:30 +0100

    added checkini method, and used it

--- src/autoderiv/rules/TreeRule.java
+++ src/autoderiv/rules/TreeRule.java
@@ -1,5 +1,6 @@
 package autoderiv.rules;
 
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
@@ -31,16 +32,13 @@ public class TreeRule implements Rule{
 		
 		// remove
 		System.out.println("TreeRule.TreeRule() for res "+path);
+		if(member!=null)
+			System.out.println("Yes !");
 	}
 
 	public void applyOnProject(IProgressMonitor progress) {
 		// initialization
-		if(member == null) 
-			member = project.findMember(path);
-		
-		// member may not exist
-		if(member == null || !member.exists()) 
-			return;
+		if(!checkIni()) return;
 		
 		// effective action
 		try {
@@ -52,6 +50,44 @@ public class TreeRule implements Rule{
 				}
 			});
 		} catch (CoreException e) { e.printStackTrace(); }
+	}
+
+	boolean checkIni(){
+		if(member == null) 
+			member = project.findMember(path);
+		return member != null && member.exists(); 
+	}
+	
+	@Override
+	public void applyOnResource(IResource res, IProgressMonitor progress) {
+		// initialization
+		if(!checkIni()) return;
+		System.out.println("TreeRule.applyOnResource() 65");
+		// check if the resource is handled by the rule
+		boolean fits = false;
+		switch(res.getType()){
+		case IResource.FILE :
+			fits = member.equals(res);
+			break;
+		case IResource.FOLDER :
+			IFolder f = (IFolder) res;
+			fits = f.findMember(res.getLocation()) != null;			
+			break;
+		default:
+			// should not happen: resource is not supposed to be a project or /
+			System.out.println("TreeRule.applyOnResource() NOT HANDLED");
+		}
+		System.out.println("TreeRule.applyOnResource() 80 "+fits);
+		
+		// the resources matches the rule, apply derived attribute
+		if(fits){
+			try {
+				res.setDerived(isDerived, progress);
+				System.out.println("TreeRule.applyOnResource() 86");
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
