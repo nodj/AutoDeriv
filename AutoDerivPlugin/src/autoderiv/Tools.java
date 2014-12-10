3e0e49371e64ab2f40fffa741e6cefbaeac70f71
johan duparc <johan.duparc@gmail.com>
2014-12-10 02:59:26 +0100

    in Tools : Added checkedSetDerived and checkedSetDerived_nt methods

--- AutoDerivPlugin/src/autoderiv/Tools.java
+++ AutoDerivPlugin/src/autoderiv/Tools.java
@@ -1,5 +1,9 @@
 package autoderiv;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
 public class Tools {
 
 	private static final long startns = System.nanoTime();
@@ -7,5 +11,21 @@ public class Tools {
 	public static double 	getmsd(){ return getns()*1e-6;}
 	public static long 		getms(){ return (long)getmsd();}
 	public static long 		getns()	{ return System.nanoTime()-startns; }
+
+
+
+	public static void checkedSetDerived(IResource res, boolean derived, IProgressMonitor progress) throws CoreException {
+		if(res.isDerived() != derived)
+			res.setDerived(derived, progress);
+	}
+
+	public static void checkedSetDerived_nt(IResource res, boolean derived, IProgressMonitor progress){
+		if(res.isDerived() != derived){
+			try {
+				res.setDerived(derived, progress);
+			} catch (CoreException e) { e.printStackTrace(); }
+		}
+	}
+
 
 }
