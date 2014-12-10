7f91a47da96840b174a0d95addfb8efdfa4f07c6
johan duparc <johan.duparc@gmail.com>
2014-12-10 02:57:55 +0100

    Updated Degbug class

--- AutoDerivPlugin/src/autoderiv/Debug.java
+++ AutoDerivPlugin/src/autoderiv/Debug.java
@@ -14,11 +14,19 @@ public class Debug {
 	public static boolean printLog = true;
 	public static ILog log;
 
+	public static boolean printDbg = false;
+	private static final String PREDBG = "d.";
 
 	/**print an info text line (clickable from eclipse console) */
 	public static void info(String str) {
 		if (printInfo){
 			common(PREINFO + str, System.out, Status.INFO);
+		}
+	}
+
+	public static void dbg(String str) {
+		if (printDbg){
+			common(PREDBG + str, System.out, Status.OK);
 		}
 	}
 
@@ -28,11 +36,11 @@ public class Debug {
 			common(PREWARN + str, System.err, Status.WARNING);
 		}
 	}
-	
+
 	private static void common(String str, PrintStream flux, int status_level) {
 		StackTraceElement s = Thread.currentThread().getStackTrace()[3];
 		String msg_ = String.format("%s:\t%s @ %s.%s(%s:%s)%n",Tools.getms(), str, s.getClassName(), s.getMethodName(), s.getFileName(), s.getLineNumber());
 		flux.print(msg_);
 	}
-	
+
 }
